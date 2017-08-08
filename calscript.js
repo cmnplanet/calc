7d266582a8773de1db1f17c47606aea4c2db6366
Christopher Nimz <cmnfv@mac.com>
2017-08-08 16:28:16 -0400

    project push

---
+++ calscript.js
@@ -0,0 +1,14 @@
+let display = document.getElementById('sum');
+
+function readout(x){
+  display.value += x;
+    if (x === "c"){
+    display.value=" ";
+  }
+}
+
+function total(){
+  x=display.value;
+  x=eval(x);
+  display.value=x;
+}
