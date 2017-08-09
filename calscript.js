9cc514d8241fef34a704b889220a9b612452ef65
Christopher Nimz <cmnfv@mac.com>
2017-08-09 16:17:03 -0400

    resubmit

--- calscript.js
+++ calscript.js
@@ -1,14 +1,17 @@
-let display = document.getElementById('sum');
+// let display = document.getElementById('sum');
+//
+// function readout(x){
+//   display.value += x;
+//     if (x === "c"){
+//     display.value=" ";
+//   }
+// }
+//
+// function total(){
+//   x=display.value;
+//   x=eval(x);
+//   display.value=x;
+// }
 
-function readout(x){
-  display.value += x;
-    if (x === "c"){
-    display.value=" ";
-  }
-}
 
-function total(){
-  x=display.value;
-  x=eval(x);
-  display.value=x;
-}
+// Not happy with any of this shortcutting on this project
